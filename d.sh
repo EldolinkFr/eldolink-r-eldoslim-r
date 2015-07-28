752e411289a14c8302cffb3d22432a3a817fe395
Alex <alexdurand@me.com>
2015-07-28 14:29:54 +0200

    Bug fix on routing file Add deployment script

---
+++ d.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Déploiement
+git pull
+composer update
+composer install --optimize-autoloader
+php -d memory_limit=256M app/console cache:clear --env=prod --no-debug
+php -d memory_limit=256M app/console assetic:dump --env=prod --no-debug
+ 
+exit 0
