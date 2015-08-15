9453e596186fbd7ae7033d574ebf5e9f851c8ea3
Alex <alexdurand@me.com>
2015-08-16 01:31:10 +0200

    Add LiipImagineBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
 //addons
 			new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
             new Bmatzner\FontAwesomeBundle\BmatznerFontAwesomeBundle(),
+			new Liip\ImagineBundle\LiipImagineBundle(),
 			new JMS\TranslationBundle\JMSTranslationBundle(),
 			new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
