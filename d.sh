639f005f4df720fb9d04517307c32c475fea8de7
Alex <alexdurand@me.com>
2015-08-26 09:56:33 +0200

    Changes on deployment script

--- d.sh
+++ d.sh
@@ -3,7 +3,7 @@ echo Déploiement
 git pull
 composer update
 composer install --optimize-autoloader
-php -d memory_limit=256M app/console cache:clear --env=prod --no-debug
+php -d memory_limit=1024M app/console cache:clear --env=prod --no-debug
 php -d memory_limit=256M app/console assetic:dump --env=prod --no-debug
  
 exit 0
