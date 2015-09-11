5a8ac35cbb4c576a227579ad1167aaad17bc96f9
Alex <alexdurand@me.com>
2015-09-11 11:20:31 +0200

    KachkaevAssetsVersionBundle integration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 //addons
 			new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new Kachkaev\AssetsVersionBundle\KachkaevAssetsVersionBundle(),
             new SmartCore\Bundle\AcceleratorCacheBundle\AcceleratorCacheBundle(),
             new Bmatzner\FontAwesomeBundle\BmatznerFontAwesomeBundle(),
 			new Liip\ImagineBundle\LiipImagineBundle(),
--- d.sh
+++ d.sh
@@ -3,6 +3,7 @@ echo Déploiement
 git pull
 composer update
 composer install --optimize-autoloader
+php -d app/console assets_version:increment
 php -d memory_limit=1024M app/console cache:clear --env=prod --no-debug
 php -d memory_limit=256M app/console assetic:dump --env=prod --no-debug
  
