0c21802db78776f987b9e3e8af5176593d33e6e7
Alexandre Durand <alexdurand@me.com>
2016-07-01 13:58:59 +0200

    Deployment script update

--- d.sh
+++ d.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-echo Déploiement
+echo "################################";
+echo "####       Déploiement      ####";
+echo "################################";
+
+dumpflag=off
+while getopts d opt
+do
+    case "$opt" in
+      d)  dumpflag=on;;
+      \?)		# unknown flag
+      	  echo >&2 \
+	  "usage: $0 [-d]"
+	  exit 1;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
 git pull
 composer update
 composer install --optimize-autoloader
 php app/console assets_version:increment
 php -d memory_limit=1024M app/console cache:clear --env=prod --no-debug
-php -d memory_limit=256M app/console assetic:dump --env=prod --no-debug
- 
+php -d memory_limit=1024M app/console cache:accelerator:clear
+
+if [ $dumpflag = "on" ]; then
+    php -d memory_limit=256M app/console assetic:dump --env=prod --no-debug
+fi
+
 exit 0
