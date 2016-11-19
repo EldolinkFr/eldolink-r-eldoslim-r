c0fc65dc075c56205b6808ef0f5876705517912d
Alex Durand <alex@eldolink.com>
2016-11-19 19:20:04 +0100

    Changes on app_dev.php file

--- web/app_dev.php
+++ web/app_dev.php
@@ -4,13 +4,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || (
-    	!in_array(@$_SERVER['HTTP_X_FORWARDED_FOR'], array('127.0.0.1', 'fe80::1', '::1', '88.179.76.43', '217.128.196.239', '78.239.178.236'))
-	    && !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '88.179.76.43', '217.128.196.239', '78.239.178.236'))
-	)
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '88.179.76.43', '217.128.196.239', '78.239.178.236')) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
-    exit('Welcome ;-) '.$_SERVER['HTTP_CLIENT_IP'].$_SERVER['REMOTE_ADDR']);
+    exit('Welcome ;-)');
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
