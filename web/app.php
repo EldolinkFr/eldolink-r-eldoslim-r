db5591201be72d8f270de29ee72659782d4d7be6
Alexandre Durand <alexdurand@me.com>
2017-05-17 11:53:04 +0200

    Set Default Timezone

--- web/app.php
+++ web/app.php
@@ -2,6 +2,8 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+date_default_timezone_set('Europe/Paris');
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../app/bootstrap.php.cache';
--- web/app_dev.php
+++ web/app_dev.php
@@ -3,6 +3,8 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
+date_default_timezone_set('Europe/Paris');
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '88.179.76.43', '217.128.196.239', '78.239.178.236')) || php_sapi_name() === 'cli-server')
