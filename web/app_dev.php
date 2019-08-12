d64e81950c09563ab74ee9eb4be1e42c07b025d3
Alex Durand <alex@eldolink.com>
2019-08-12 17:54:23 +0200

    Minor changes

--- web/app_dev.php
+++ web/app_dev.php
@@ -6,8 +6,7 @@ use Symfony\Component\Debug\Debug;
 date_default_timezone_set('Europe/Paris');
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '88.179.76.43', '217.128.196.239', '78.239.178.236')) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['HTTP_X_FORWARDED_FOR'], array('127.0.0.1', 'fe80::1', '::1', '88.179.76.43', '217.128.196.239', '78.239.178.236')) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('Welcome ;-)');
