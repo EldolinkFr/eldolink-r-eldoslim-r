de3d89d58e6338e892b869fe06ae0323a7e1495b
Alex <alex@eldolink.com>
2021-12-23 13:17:30 +0100

    Changes on app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -5,16 +5,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 date_default_timezone_set('Europe/Paris');
 
-/*
-exec('rm -rf '.__DIR__.'/../app/logs/');
-exec('rm -rf '.__DIR__.'/../app/cache/d*');
-exec('rm -rf '.__DIR__.'/../app/cache/p*');
-die;
-*/
-
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '78.193.220.199', '82.64.108.167')) || php_sapi_name() === 'cli-server')
+    || ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !(in_array(@$_SERVER['HTTP_X_FORWARDED_FOR'], array('127.0.0.1', '::1', '78.193.220.199', '82.64.108.167'), true) || PHP_SAPI === 'cli-server') )
+    || ( !isset($_SERVER['HTTP_X_FORWARDED_FOR']) && isset($_SERVER['REMOTE_ADDR']) && !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '78.193.220.199', '82.64.108.167'), true) || PHP_SAPI === 'cli-server') )
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('Welcome ;-)');
