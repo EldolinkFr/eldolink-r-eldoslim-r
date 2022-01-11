8bb56c7843d91cf018f2712a758a65af67ca412e
Alex <alex@eldolink.com>
2022-01-11 16:27:46 +0000

    Grunt changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,16 +24,47 @@ module.exports = function (grunt) {
             options: {
                 stripBanners: true
             },
+            app_general_css: {
+                src: [
+            		'web/bundles/eldotools/css/bootstrap-modal.css',
+            		'web/bundles/eldotools/css/animate.css',
+            		'web/bundles/eldotools/css/custom.css',
+            		'web/bundles/eldotools/css/notify.css'
+                ],
+                dest: 'web/css/t.css',
+                nonull: true,
+            },
+            app_general_js : {
+                src : [
+            		'web/bundles/eldotools/js/jquery.cookie.js',
+            		'web/bundles/eldotools/js/bootstrap.js',
+            		'web/bundles/eldotools/js/plugins/bootstrap-notify/bootstrap-notify.js',
+            		'web/bundles/eldotools/js/plugins/jquery-validate/jquery.validate.min.js',
+            		'web/bundles/eldotools/js/plugins/bounceback.eldo.js',
+            		'web/bundles/eldotools/js/general.js'
+                ],
+                dest: 'web/js/t.js',
+                nonull: true,
+            },
         },
         cssmin : {
             errors:{
                 src: 'web/css/errors.css',
                 dest: 'web/assets/css/errors.min.css'
-            }
+            },
+            app_general_css:{
+                src: 'web/css/t.css',
+                dest: 'web/css/t.css'
+            },
         },
         uglify : {
+            app_general_js: {
+                files: {
+                    'web/js/t.js': ['web/js/t.js']
+                }
+            },
         },
     });
 
-    grunt.registerTask('default', ['bowercopy', 'cssmin']);
+    grunt.registerTask('default', ['bowercopy', 'concat', 'cssmin', 'uglify']);
 };
